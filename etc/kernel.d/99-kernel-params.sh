77d4a433b23f46d09cdf72c47e244215c8885373
aniruddhgutta <aniruddhgutta@disroot.org>
2026-05-25 08:10:53 +0530

    kernel.d: reduce loglevel

--- etc/kernel.d/99-kernel-params.sh
+++ etc/kernel.d/99-kernel-params.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # set kernel-params
 
+params="nowatchdog nmi_watchdog=0 mitigations=off loglevel=3"
+
 printf "setting kernel-params...\n"
 for kver in "$@"; do
     f="/boot/loader/entries/chimera-${kver}.conf"
     [ -f "$f" ] || continue
-    sed 's/^options .*/& nowatchdog nmi_watchdog=0/' "$f" > "${f}_"
+    sed "s/^options .*/& $params/" "$f" > "${f}_"
     mv "${f}_" "$f"
 done
